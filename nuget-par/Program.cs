f118d2f252d6cb0e28041c7aae18dd867aab0a54
jonathan.russell@nice.org.uk <jonathan.russell@nice.org.uk>
2026-05-12 12:29:00 +0100

    initial commit

---
+++ nuget-par/Program.cs
@@ -0,0 +1,8 @@
+﻿using Newtonsoft.Json;
+using Raven.Abstractions.Data;
+
+var test = new RavenConnectionStringOptions(){};
+string json = JsonConvert.SerializeObject("123", Formatting.Indented);
+
+
+Console.WriteLine("PAR nuget test project");
